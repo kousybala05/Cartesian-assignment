3d2805c532b4e6d178692ec96c4ccae2a2417cff
Kousalya <91832403+kousybala05@users.noreply.github.com>
2022-08-28 17:42:46 +0530

    Add files via upload

---
+++ cartesian assignment/assignment.sql
@@ -0,0 +1,16 @@
+create database database1;
+use database1;
+ 
+ CREATE TABLE Customer (Customer_Id varchar(255));
+ INSERT INTO Customer VALUES('Customer_Id'),('Abhinash'),('Vipin'),('Mahesh'),('Bijoy'),('Bhabani'),('Ashutosh');
+    
+ CREATE TABLE Voucher (Voucher_Id varchar(255) UNIQUE);
+ INSERT INTO Voucher VALUES('ABXFH'),('SDFGH'),('ERTYY'),('PPLKM')
+    
+ ;with cte 
+ as(select *,row_number() over(order by Customer_Id) rr from Customer)
+ ,cte2 as(select *,row_number() over(order by Voucher_Id) rr from Voucher)
+    
+ select Customer_Id Customer_Key,Voucher_Id Gift_Voucher_Key
+ from cte c1
+ join cte2 c2 on c1.rr=c2.rr
